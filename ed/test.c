683622cf68b60fd22a1dd427cf27234f433ba970
aluno-uffs <Aluno UFFS>
2018-08-23 17:40:01 -0300

    things

---
+++ ed/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int mdc (int a, int b) {
+
+    if (b == 0) return a;
+    return mdc(b, a % b);
+}
+
+int main () {
+
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+    printf("%d\n", mdc(a, b));
+
+    return 0;
+}
