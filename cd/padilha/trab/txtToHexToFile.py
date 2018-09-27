69f380984c54908f3be4da73b36c435839132721
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-09-26 21:33:12 -0300

    asd

--- cd/padilha/trab/txtToHexToFile.py
+++ cd/padilha/trab/txtToHexToFile.py
@@ -1,10 +1,12 @@
-s = input()
+import brailledots
+
+s = input().upper()
 f = open("in.txt", "w")
 
 print("v2.0 raw", file=f)
 
 for x in s:
-    print(hex(ord(x))[2:], end=" ", file=f)
+    print(hex(int(brailledots.dots[hex(ord(x))[2:]], 2))[2:], end=" ", file=f)
 
-print(file=f)    
+print(file=f)
 f.close()
