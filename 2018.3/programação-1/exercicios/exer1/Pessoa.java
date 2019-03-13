467d4f33ff484facba5476f3fb6a63488c69cb90
Diego Becker <diegoluizbecker@gmail.com>
2019-03-13 11:27:23 -0300

    Atividade toda cagada feita com o cu

---
+++ 2018.3/programação-1/exercicios/exer1/Pessoa.java
@@ -0,0 +1,34 @@
+class Pessoa {
+
+    private int idade;
+    private int anoNascimento;
+    private double altura;
+
+    public Pessoa() {
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int newIdade) {
+        this.idade = newIdade;
+    }
+
+    public int getAnoNascimento() {
+        return this.anoNascimento;
+    }
+
+    public void setAnoNscimento(int newAnoNascimento) {
+        this.anoNascimento = newAnoNascimento;
+    }
+
+    public double getAtura() {
+        return this.altura;
+    }
+
+    public void setAltura(double newAltura) {
+        this.altura = newAltura;        
+    }
+}
+
---
+++ 2018.3/programação-1/testes/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String args[]) {
+        Data a, b;
+        a = new Data();
+        b = new Data();
+        a.mes = 10;
+
+        System.out.println(a.mes);
+    }
+}
