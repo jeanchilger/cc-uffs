20c2a1ed1e51de30d585823a045d86d13c4845a2
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2019-10-22 17:00:00 -0300

    adds bfs and dfs

--- 2019.2/grafos/buscas/search.cpp
+++ 2019.2/grafos/buscas/search.cpp
@@ -1,9 +1,132 @@
 #include <stdio.h>
+#include <deque>
+#include <vector>
+
 #include <search.h>
+
+using namespace std;
+
+bool is_in(vector<int> array, int valor) {
+    for (int x : array) {
+        if (x == valor) {
+            return true;
+        }
+    }
+
+    return false;
+}
+bool is_ina(deque<int> array, int valor) {
+    for (int x : array) {
+        if (x == valor) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void print_fronteira(deque<int> fronteira) {
+    printf("FRONTEIRA: [");
+    for (int x : fronteira) {
+        printf("%d, ", x);
+    }
+
+    printf("]\n");
+}
+
+// busca largura
+bool busca_largura(vector< vector<int> > grafo, int inicio, int fim) {
+    printf("Busca em largura\n");
+    deque<int> fronteira;
+
+    vector<int> visitados;
+    fronteira.push_back(inicio);
+
+    while (!fronteira.empty()) {
+        print_fronteira(fronteira);
+
+        if (fronteira.front() == fim) {
+            return true;
+
+        } else {
+            int noPai = fronteira.front();
+            fronteira.pop_front();
+            visitados.push_back(noPai);
+
+            int no = 0;
+            for (int n : grafo[noPai]) {
+                if (n == 1) {
+                    if (!is_in(visitados, no)) {
+                        fronteira.push_back(no);
+                    }
+                }
+
+                no++;
+            }
+
+        }
+    }
+
+    return false;
+}
+
+// busca profundidade
+bool busca_profundidade(vector< vector<int> > grafo, int inicio, int fim) {
+    printf("Busca em profundidade\n");
+    deque<int> fronteira;
+
+    vector<int> visitados;
+    fronteira.push_front(inicio);
+
+    while (!fronteira.empty()) {
+        print_fronteira(fronteira);
+
+        if (fronteira.front() == fim) {
+            return true;
+
+        } else {
+            int noPai = fronteira.front();
+            fronteira.pop_front();
+            visitados.push_back(noPai);
+
+            int no = 0;
+            for (int n : grafo[noPai]) {
+                if (n == 1) {
+                    if (!is_in(visitados, no) && !is_ina(fronteira, no)) {
+                        fronteira.push_front(no);
+                    }
+                }
+
+                no++;
+            }
+
+        }
+    }
+
+    return false;
+}
 
 
 
 int main() {
+
+    int n; // linhas
+    int m; // colunas
+    int v;
+
+    scanf(" %d %d", &n, &m);
+
+    vector<vector<int>> grafo(n, vector<int>(m, 0));
+
+    for (int i=0; i < n; i++) {
+        for (int j=0; j < m; j++) {
+            scanf(" %d", &v);
+            grafo[i][j] = v;
+        }
+    }
+
+    printf("MIAU: %d\n", busca_largura(grafo, 0, 3));
+    printf("MIAU: %d\n", busca_profundidade(grafo, 0, 3));
 
     return 0;
 }
--- 2019.2/grafos/buscas/search.h
+++ 2019.2/grafos/buscas/search.h
@@ -1,6 +1,6 @@
 #ifndef
 #define _SEARCH_9813_
 
-int busca_largura(int[][], char, char);
+int busca_largura(vector< vector<int> >, int, int);
 
 #endif
