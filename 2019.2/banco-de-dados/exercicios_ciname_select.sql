4cb9822f37451fe3d1f0b9a76538cb2e97f38154
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2019-10-23 13:58:50 -0300

    updates db exercises

--- 2019.2/banco-de-dados/exercicios_ciname_select.sql
+++ 2019.2/banco-de-dados/exercicios_ciname_select.sql
@@ -1,3 +1,6 @@
+---- 4)
+SELECT f.titulo, p.nome FROM filme f JOIN pessoa p on f.codd=p.codp;
+
 ---- 5)
 select f.titulo from filme f where f.codd=101;
 
@@ -18,9 +21,23 @@ SELECT f.titulo FROM filme f JOIN avaliacao a ON f.codf=a.codf WHERE a.nestrelas
 SELECT f.titulo FROM filme f JOIN elenco e ON f.codf=e.codf 
                              JOIN pessoa p ON p.codp=e.codp WHERE p.nome='Steven';
 
--- 10)
+---- 10)
+SELECT p.nome, f.titulo FROM elenco e NATURAL JOIN pessoa p
+                                      JOIN filme f ON e.codf=f.codf;
 
--- 11)
+---- 11)
+SELECT p.nome FROM pessoa p WHERE p.nome NOT IN (SELECT p.nome FROM elenco e NATURAL JOIN pessoa p);
+
+---- 12)
+SELECT * FROM filme f LEFT JOIN avaliacao a ON f.codf=a.codf WHERE a.codf IS NULL;
+
+---- 13)
+SELECT f.titulo, f.dur FROM filme f
+                       WHERE f.anol BETWEEN 1960 AND 1969;
+
+---- 14)
+SELECT p.nome FROM pessoa p JOIN elenco e ON e.codp=p.codp
+              WHERE e.papel = 'figur';
 
 ---- 15)
 SELECT count(a.coment) FROM avaliacao a WHERE a.email = 'cinefilo@gmail.com';
