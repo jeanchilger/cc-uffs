98609b953121803764e7782041b802e301b75c2c
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2021-03-07 17:40:36 -0300

    chore: create assignment-2

---
+++ bd2/assignment-2/]
@@ -0,0 +1,49 @@
+# Atividade 1 - Banco de Dados II
+
+## Descrição da Atividade
+
+Adaptado da tarefa original.
+
+**Atividade 1**
+
+Este trigger deve cuidar do armazenamento de lugares livre de  voos. O trigger é disparado quando ocorre uma inserção de um passageiro em um voo (Bookings). Os efeitos devem ser os seguintes: 
+1.1 - Se o número de poltronas do voo é positiva (AvailableFlights), deve ser decrementado;
+1.2 - Se não existir poltronas, a emissão do ticket falha;
+1.3 - Se a emissão do ticket for ok, é necessário incrementar o preço da passagem em 50 reais para o próximo passageiro
+OBS: para inserir uma linha no booking antes deve-se inserir o voo na tabela AvailableFlights
+
+Esquema do banco: 
+
+```
+available_flights(
+    num_flight int,
+    date date,
+    number_of_free_seats int,
+    price float
+)
+
+bookings(
+    num_flight int,
+    date date,
+    passenger int,
+    price float
+)
+```
+
+**Atividade 2**
+
+A partir do banco de dados hw2 (que deve ser carregado do arquivo db.sql presente no moodle), construa gatilhos para as seguintes funcionalidades:
+2.1 - se o produto em estoque chegar ao valor 0 (não tem mais o produto) então o mesmo deve ser removido das tabelas Product e Stock. Tem um esqueleto de exemplo. 
+ 
+2.2 - Para evitar problemas com uso indevido de medicamentos controlados, crie uma tabela auxiliar de  controle para verificar quem está requisitando cada medicamento com a data e hora. 
+
+
+## Solução A1
+
+Os comandos são executados com o usuário padrão `postgres`, utilizando o banco `task_two`.
+
+Para criar o banco excute o seguinte comando:
+
+```
+sudo -u postgres psql -f create-populate-table.sql
+```
---
+++ bd2/assignment-2/create-populate-table.sql
@@ -0,0 +1,88 @@
+-- Creates a database and connects to it
+SELECT 'CREATE DATABASE task_one'
+WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'task_one'
+) \gexec
+
+\c task_one
+
+
+-- Creates an 'employees' table.
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    birth_date DATE NOT NULL,
+    wage FLOAT NOT NULL
+);
+
+
+-- Helper function for random text generation.
+-- Code from https://www.simononsoftware.com/random-string-in-postgresql
+-- Credits to Szymon Lipiński
+CREATE OR REPLACE FUNCTION 
+    random_text(length INT) 
+RETURNS TEXT AS $$
+DECLARE
+    possible_chars TEXT := '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    output TEXT := '';
+    i INT4;
+    chars_size INTEGER := length(possible_chars);
+BEGIN
+    chars_size := length(possible_chars);
+    FOR i IN 1..length LOOP 
+        output := output || substr(
+            possible_chars, 
+            (1 + FLOOR((chars_size - $1 + 1) * random() ))::INTEGER, 1);
+    END LOOP;
+
+    RETURN output;
+END; 
+$$ LANGUAGE plpgsql;
+
+
+-- Helper function to insert a single employee
+CREATE OR REPLACE FUNCTION 
+    insert_employee(
+        id INTEGER
+    ) 
+RETURNS VOID AS $$
+DECLARE
+    birth_date DATE;
+    wage FLOAT;
+    name VARCHAR(50);
+BEGIN
+    -- Get random values to insert
+    SELECT random() * (4000 - 1000) + 1000 INTO wage;
+    SELECT timestamp '1970-01-01' + random() * (timestamp '2002-12-31' - timestamp '1970-01-01') 
+        INTO birth_date;
+    SELECT random_text((random() * 10 + 3)::INTEGER) INTO name;
+     
+    INSERT INTO employees VALUES (id, name, birth_date, wage);
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Function to create 'n' employees.
+CREATE OR REPLACE FUNCTION
+    populate_employees(
+        n INTEGER
+    )
+RETURNS VOID AS $$
+DECLARE
+    i INT4;
+BEGIN
+    i := 0;
+    LOOP
+        i:= i + 1;
+        PERFORM insert_employee(i);
+        EXIT WHEN i = n;
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT populate_employees(5);
+
+-- Adds an additional employee for easy control.
+INSERT INTO employees VALUES (6, 'Control Emp', current_date, 100);
