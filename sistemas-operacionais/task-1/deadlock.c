432eeb5cc1cd71cb115c6d0849a4cb1cf81ec64a
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2021-03-22 22:55:06 -0300

    feat(os): adds task1 solution.

--- sistemas-operacionais/task-1/deadlock.c
+++ sistemas-operacionais/task-1/deadlock.c
@@ -1,6 +1,13 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#define RED     "\e[0;31m"
+#define RED_BOLD "\e[1;91m"
+#define GREEN   "\e[0;32m"
+#define GREEN_BOLD "\e[1;92m"
+#define RESET   "\e[0m"
 
 void *thread1(void *data);
 void *thread2(void *data);
@@ -15,9 +22,10 @@ int main(void) {
 
     // now join them
     pthread_join(Thread1, NULL);
-    printf("Thread id %ld returned\n", Thread1);
+    printf(GREEN "THREAD 1 id %ld returned\n" RESET, Thread1);
+
     pthread_join(Thread2, NULL);
-    printf("Thread id %ld returned\n", Thread2);
+    printf(RED "THREAD 2 id %ld returned\n" RESET, Thread2);
 
     return 1;
 }
@@ -25,32 +33,39 @@ int main(void) {
 void *thread1(void *data) {
     unsigned long i, j;
 
-    while (pthread_mutex_trylock(&mutex1) != 0 &&
-            pthread_mutex_trylock(&mutex2) != 0) {}
+    while (1) {
+        if (pthread_mutex_trylock(&mutex1) == 0) {
+            printf(GREEN "THREAD 1 (%ld) " RESET "got mutex1.\n", pthread_self());
 
-    if (pthread_mutex_lock(&mutex1) == 0) {
-        printf("Thread ID%ld got mutex1.\n", pthread_self());
+            // Just for wasting some time
+            printf("\tWait...\n");
+            sleep(2);
 
-        for (i=0; i < 10000000; ++i) {}  // just for wasting some time
+            if (pthread_mutex_lock(&mutex2) == 0) {
+                printf(GREEN "THREAD 1 (%ld) " RESET "got mutex2.\n", pthread_self());
 
-        if (pthread_mutex_trylock(&mutex2) == 0) {
-            printf("Thread ID%ld got mutex2.\n", pthread_self());
+                // Just for wasting some time
+                printf("\tWait...\n");
+                sleep(2);
 
-            for (i=0; i< 10000000; ++i) {}  // just for wasting some time
+                printf(RED "THREAD 1 (%ld) " RESET "consumed both.\n", pthread_self());
 
-            pthread_mutex_unlock(&mutex2);
+                pthread_mutex_unlock(&mutex2);
+                pthread_mutex_unlock(&mutex1);
+                
+                break;
+
+            } else {
+                printf(GREEN "THREAD 1 (%ld) " RESET "did not get mutex2.\n", pthread_self());
+                pthread_mutex_unlock(&mutex1);
+            }
 
         } else {
-            printf("Thread ID%ld did not get mutex2.\n", pthread_self());
-            pthread_mutex_unlock(&mutex2);
+            printf(GREEN "THREAD 1 (%ld) " RESET "did not get mutex1.\n", pthread_self());
         }
-
-        pthread_mutex_unlock(&mutex1);
-
-    } else {
-        printf("Thread ID%ld did not get mutex1.\n", pthread_self());
-        pthread_mutex_unlock(&mutex1);
     }
+
+    printf(GREEN_BOLD "THREAD 1 (%ld) COMPLETE.\n" RESET, pthread_self());
 
     pthread_exit(NULL);
 }
@@ -58,29 +73,41 @@ void *thread1(void *data) {
 void *thread2(void *data) {
     unsigned long i, j;
 
-    if (pthread_mutex_lock(&mutex2) == 0) {
-        printf("Thread ID%ld got mutex2.\n", pthread_self());
+    while (1) {
+        if (pthread_mutex_lock(&mutex2) == 0) {
+            printf(RED "THREAD 2 (%ld) " RESET "got mutex2.\n", pthread_self());
 
-        for (i=0; i< 10000000; ++i) {}  // just for wasting some time
+            // Just for wasting some time
+            printf("\tWait...\n");
+            sleep(2);
 
-        if (pthread_mutex_trylock(&mutex1) == 0) {
-            printf("Thread ID%ld got mutex1.\n", pthread_self());
+            if (pthread_mutex_trylock(&mutex1) == 0) {
+                printf(RED "THREAD 2 (%ld) " RESET "got mutex1.\n", pthread_self());
 
-            for (i=0; i< 10000000; ++i) {}  // just for wasting some time
+                // Just for wasting some time
+                printf("\tWait...\n");
+                sleep(2);
 
-            pthread_mutex_unlock(&mutex1);
+                printf(RED "THREAD 2 (%ld) " RESET "consumed both.\n", pthread_self());
+
+                pthread_mutex_unlock(&mutex1);
+                pthread_mutex_unlock(&mutex2);
+
+                break;
+
+            } else {
+                printf(RED "THREAD 2 (%ld) " RESET "did not get mutex1.\n", pthread_self());
+                pthread_mutex_unlock(&mutex2);
+                sleep(2);
+
+            }
 
         } else {
-            printf("Thread ID%ld did not get mutex1.\n", pthread_self());
-            pthread_mutex_unlock(&mutex1);
+            printf(RED "THREAD 2 (%ld) " RESET "did not get mutex2.\n", pthread_self());
         }
-
-        pthread_mutex_unlock(&mutex2);
-
-    } else {
-        printf("Thread ID%ld did not get mutex2.\n", pthread_self());
-        pthread_mutex_unlock(&mutex2);
     }
 
+    printf(RED_BOLD "THREAD 2 (%ld) COMPLETE.\n" RESET, pthread_self());
+    
     pthread_exit(NULL);
 }
