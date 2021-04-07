fce431efb45d4837b103f73b0fba3ae02bd07295
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2021-04-07 00:44:56 -0300

    feat(so): adds task-2 solution.

---
+++ sistemas-operacionais/task-2/dining_philosophers.c
@@ -0,0 +1,127 @@
+#include "dining_philosophers.h"
+
+void * philosopherCycle(void *data) {
+    int philosopherId = *((int *) data);
+
+    while (1) {
+        eat(philosopherId);
+        
+        think(philosopherId);
+
+        break;
+    }
+
+    pthread_exit(NULL);
+}
+
+// Utility functions to fork acquisition.
+//
+int leftPhilosopher(int philosopherId) {
+    return (philosopherId + N - 1) % N;
+}
+
+int rightPhilosopher(int philosopherId) {
+    return (philosopherId + 1) % N;
+}
+
+void checkNeighbors(int philosopherId) {
+    if (state[philosopherId] == HUNGRY &&
+        state[leftPhilosopher(philosopherId)] != EATING &&
+        state[rightPhilosopher(philosopherId)] != EATING
+    ) {
+        state[philosopherId] = EATING;
+        printf(
+            "Philosopher " WHITE_BOLD "%d " RESET "is" GREEN_BOLD " EATING\n\n" RESET,
+            philosopherId
+        );
+
+        sem_post(&philosophers[philosopherId]);
+    }
+}
+
+void getForks(int philosopherId) {
+    sem_wait(&changeState);
+
+    state[philosopherId] = HUNGRY;
+    printf(
+        "Philosopher " WHITE_BOLD "%d " RESET "is" RED_BOLD " HUNGRY\n" RESET,
+        philosopherId
+    );
+
+    // Check if philosopher may eat based on neighbors' states
+    checkNeighbors(philosopherId);
+
+    sem_post(&changeState);
+
+    // If philosopher couldn't eat, this down operation will set
+    // his semaphore to 0.
+    sem_wait(&philosophers[philosopherId]);
+}
+
+void putForks(int philosopherId) {
+    sem_wait(&changeState);
+
+    state[philosopherId] = THINKING;
+    printf(
+        "Philosopher " WHITE_BOLD "%d " RESET "is" BLUE_BOLD " THINKING\n" RESET,
+        philosopherId
+    );
+
+    // Since `philosopherId` just ended eating, if any of his
+    // neighbors are HUNGRY (and need only one of the forks 
+    // used by `philosopherId`) the fork will be given to him.
+    checkNeighbors(rightPhilosopher(philosopherId));
+    checkNeighbors(leftPhilosopher(philosopherId));
+    
+    sem_post(&changeState);
+}
+
+// Phlosopher's cycle actions
+//
+void eat(int philosopherId) {
+
+    // sleep(1);
+
+    getForks(philosopherId);
+
+    sleep(1);
+
+    putForks(philosopherId);
+
+    sleep(1);
+
+}
+
+void think(int philosopherId) {
+    sleep(1);
+}
+
+int main() {
+    pthread_t philosopherThreads[N];
+    int philosopherIds[N];
+    
+    sem_init(&changeState, 0, 1);
+
+    for (int i=0; i < N; i++) {
+        sem_init(&philosophers[i], 0, 0);
+        state[i] = THINKING;
+        philosopherIds[i] = i;
+    }
+
+    // Initializes philosophers' life cycles.
+    for (int i=0; i < N; i++) {
+        pthread_create(
+            &philosopherThreads[i],
+            NULL,
+            philosopherCycle,
+            (void *)&philosopherIds[i]
+        );
+    }
+
+    // Join threads
+    for (int i=0; i < N; i++) {
+        pthread_join(philosopherThreads[i], NULL);
+    }
+
+    return 0;
+}
---
+++ sistemas-operacionais/task-2/dining_philosophers.h
@@ -0,0 +1,75 @@
+//
+// Exec:
+//     gcc -o philosophers dining_philosophers.c -lpthread && ./philosophers 
+
+#ifndef _H_DINING_PHILOSOPHERS_
+#define _H_DINING_PHILOSOPHERS_
+
+#include <pthread.h>
+#include <semaphore.h>      
+#include <stdio.h>
+#include <unistd.h>
+
+#define N 5
+
+sem_t philosophers[N];
+sem_t changeState;
+
+enum {
+    THINKING,
+    EATING,
+    HUNGRY
+} state[N];
+
+// Helpers for colorful printfs
+#define RED          "\e[0;31m"
+#define RED_BOLD     "\e[1;91m"
+#define WHITE_BOLD   "\e[1;37m"
+#define GREEN        "\e[0;32m"
+#define GREEN_BOLD   "\e[1;92m"
+#define BLUE_BOLD   "\e[1;94m"
+#define RESET        "\e[0m"
+
+/**
+ * Represents a Philosopher's life cycle. 
+ */
+void * philosopherCycle(void *data);
+
+/**
+ * Phlosopher's cycle actions. Essentially, just a
+ * sleep, to spend some time.
+*/
+void eat(int philosopherId);
+
+void think(int philosopherId);
+
+/**
+ * Utility funcitons to obtain fork number based on
+ * philosopher id.
+ */
+int leftPhilosopher(int philosopherId);
+
+int rightPhilosopher(int philosopherId);
+
+/**
+ * Get both the forks for philosopher `philosopherId`. If only
+ * one of them is available, set the philosopher as "HUNGRY".
+ */
+void getForks(int philosopherId);
+
+/**
+ * Put both forks on the table. If any neighboring philosopher
+ * is "HUNGRY", the fork is given to him.
+ */
+void putForks(int philosopherId);
+
+/**
+ * Checks the states of `philosopherId`'s neighbors.
+ *
+ * If `philosopherId` is HUNGRY and it's neighbors are not
+ * eating, then he may eat (e.g. the corresponding semaphore may
+ * be "comsumed").
+ */
+void checkNeighbors(int philosopherId);
+
+#endif
