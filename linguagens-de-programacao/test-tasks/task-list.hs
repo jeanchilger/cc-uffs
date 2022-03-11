82b16f5b30ac4092b9d5152cd3a65f6048de26ce
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2022-03-11 02:31:26 -0300

    feat: Adds test exercices

--- linguagens-de-programacao/test-tasks/task-list.hs
+++ linguagens-de-programacao/test-tasks/task-list.hs
@@ -1,3 +1,4 @@
+import Data.Text.Internal.Fusion.Common (findBy)
 -------------------------------------------------------
 -- 1
 -------------------------------------------------------
@@ -65,9 +66,110 @@ prod3 :: Double -> Double -> Double -> Double
 prod3 a b c = a * b * c
 
 -------------------------------------------------------
--- 5
+-- 6
 -------------------------------------------------------
 idealHeight :: Double -> Char -> Double
 idealHeight height sex = if sex == 'm'
     then 72.7 * height - 58
     else 62.1 * height - 44.7
+
+-------------------------------------------------------
+-- 21
+-------------------------------------------------------
+and' :: Bool -> Bool -> Bool
+and' False _ = False
+and' _ False = False
+and' True True = True
+
+-------------------------------------------------------
+-- 24
+-------------------------------------------------------
+findByName :: [(String, String)] -> String -> [(String, String)]
+findByName list name = filter (\tuple -> fst tuple == name) list
+
+
+-------------------------------------------------------
+data Cliente =
+    OrgGov String
+    | Empresa String Integer String String
+    | Individuo Pessoa' Bool
+    deriving Show
+
+data Pessoa' =
+    Pessoa' String String
+    deriving Show
+
+getNameCliente :: Cliente -> String
+getNameCliente (OrgGov name) = name
+getNameCliente (Empresa name _ _ _) = name
+getNameCliente (Individuo (Pessoa' name _) _) = name
+-------------------------------------------------------
+-------------------------------------------------------
+fat :: Integer -> Integer
+fat n
+    | n == 0 = 1
+    | n > 0 = n * fat (n - 1)
+-------------------------------------------------------
+-------------------------------------------------------
+divide :: Integer -> Integer -> Integer
+divide a b
+    | a >= b = 1 + divide (a - b) b
+    | a < b = 0
+-------------------------------------------------------
+-------------------------------------------------------
+len :: [Integer] -> Integer
+len list
+    | null list = 0
+    | otherwise = 1 + len (tail list)
+
+len' :: [Integer] -> Integer
+len' [] = 0
+len' (x:xs) = 1 + len' xs
+
+get :: Integer -> [Integer] -> Integer
+get 0 (x:xs) = x
+get nth (x:xs) = get (nth - 1) xs
+
+cat :: [Integer] -> [Integer] -> [Integer]
+cat xs b = foldr (:) b xs
+-------------------------------------------------------
+-------------------------------------------------------
+data Pessoa = Pessoa String String Integer
+    deriving Show
+
+data Estudante =
+    EstudanteMedio Pessoa [Double]
+    | EstudanteSuperior Pessoa Integer [Double]
+    | EstudanteMestrado Pessoa String [Double]
+    deriving Show
+
+--
+
+getNome :: Estudante -> String
+getNome (EstudanteMedio (Pessoa nome sobrenome _) _) = nome ++ " " ++ sobrenome
+getNome (EstudanteSuperior (Pessoa nome sobrenome _) _ _) = nome ++ " " ++ sobrenome
+getNome (EstudanteMestrado (Pessoa nome sobrenome _) _ _) = nome ++ " " ++ sobrenome
+
+getTemaDissertacao :: Estudante -> String
+getTemaDissertacao (EstudanteMestrado _ tema _) = tema
+getTemaDissertacao (EstudanteMedio _ _) = ""
+getTemaDissertacao (EstudanteSuperior {}) = ""
+
+getNotas :: Estudante -> [Double]
+getNotas (EstudanteMedio _ notas) = notas
+getNotas (EstudanteSuperior _ _ notas) = notas
+getNotas (EstudanteMestrado _ _ notas) = notas
+
+--
+data Data = Data Integer Integer Integer
+    deriving Show
+
+addDay :: Data -> Data
+addDay (Data d m y)
+    | d + 1 > 30 = addDay (Data 0 (m + 1) y)
+    | m > 12 = addDay (Data d 1 (y + 1))
+    | otherwise = Data (d + 1) m y
+
+-------------------------------------------------------
+-------------------------------------------------------
+
